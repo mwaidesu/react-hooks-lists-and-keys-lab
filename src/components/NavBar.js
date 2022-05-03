f30fca5320b495295b2a6f14f99a06f9860c7773
Mwai <machariamwai@gmail.com>
2022-05-03 18:38:56 +0300

    Pass all tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let aTag = links.map((link) =>(
+    <a key={link} href={"#" + link}>{link}</a>
+  ));
+
+  return <nav>{aTag}</nav>;
 }
 
 export default NavBar;
